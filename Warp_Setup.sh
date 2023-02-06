1846e2726dbbca089bca8a9dc91e8d375df82ac3
Shishir1035 <shishirshohanuzzaman@gmail.com>
2023-02-06 10:43:52 -0500

    issue fixed v1.1

--- Warp_Setup.sh
+++ Warp_Setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash 
-echo "Warning! The file is tested on Ubuntu 22.04(jammy) only."
-echo "-------------------------------------------------------------------------------"
-echo "If the programme pauses at any second press enter. Should continue setting up."
-echo "-------------------------------------------------------------------------------"
+echo "----------------------------------------------------------------------------------------------------------------"
+echo "Warning! The file is tested on Ubuntu 22.04(jammy) & Kali Linux only. Should work on other debian distros too. "
+echo "----------------------------------------------------------------------------------------------------------------"
+echo " "
 echo "[+] Downloading Warp. Please wait!"
 sudo wget https://pkg.cloudflareclient.com/uploads/cloudflare_warp_2023_1_133_1_amd64_dc941b82de.deb
 sleep 2
@@ -13,8 +13,8 @@ echo "[+] Fixing errors!!"
 sudo apt --fix-broken install -y
 echo "[+] Almost there!!"
 warp-cli register
-read permit
 echo "[+] To connect with warp simply write : 'warp-cli connect'"
+sleep 2
 echo "[+] Deleting .deb file....."
 sudo rm cloudflare_warp_2023_1_133_1_amd64_dc941b82de.deb
-echo "Have Fun"
+echo "Have Fun!!!"
